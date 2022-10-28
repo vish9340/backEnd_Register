15e403d25bca080b7d57cffd4ccec737fa065b96
vish9340 <108508334+vish9340@users.noreply.github.com>
2022-10-28 03:28:22 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+var jsonParser = bodyParser.json()
 
 
 
@@ -12,9 +13,9 @@ const { Authenticate } = require("./middlewares/auth.mid");
 app.get("/", (req, res)=>{return res.status(200).send("Hello")});
 const AddController = require("./controllers/Add.controller");
 const UserCOntroller = require("./controllers/user.controller");
-app.use("/data", AddController);
-app.use("/users", UserCOntroller);
-app.post("/register", register);
-app.post("/login", login);
+app.use("/data",jsonParser, AddController);
+app.use("/users",jsonParser, UserCOntroller);
+app.post("/register",jsonParser, register);
+app.post("/login",jsonParser, login);
 
 module.exports = app;
